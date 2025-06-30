bef799b666622f96218f92b3ca8f4f40bf286a17
Guilherme B. Stella <120825800+Guilhermestella29@users.noreply.github.com>
2025-06-30 13:09:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+esp_ip = "http://192.168.175.114/"
+
+try:
+    response = requests.get(esp_ip, timeout=2)
+    st.write("Resposta do ESP:", response.text)
+except requests.exceptions.RequestException as e:
+    st.write("Erro ao conectar ao ESP:", e)
